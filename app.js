38e106c4bd8cedf517786a1acc7facf77c9979ee
diegohorovitz <horovitzdiego@gmail.com>
2021-07-20 17:40:32 -0300

    diego

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ app.listen (3000, ()=>{
     console.log('Servidor corriendo en el puerto 3000')
 });
 
-app.get('/index.html', (req,res)=>{
+app.get('/index', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 })
 
-app.get('/login.html', (req,res)=>{
+app.get('/login', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
 
-app.get('/productCart.html', (req,res)=>{
+app.get('/productCart', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/productCart.html'))
 })
 
-app.get('/productDetail.html', (req,res)=>{
+app.get('/productDetail', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
 })
 
-app.get('/register.html', (req,res)=>{
+app.get('/register', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
